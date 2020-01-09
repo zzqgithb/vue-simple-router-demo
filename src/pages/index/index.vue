c8e6e71502f4a7a76b7f09d487e8443a28591d5b
翟梓钦 <zhaizq@ketr.com.cn>
2020-01-09 19:08:07 +0800

    项目框架

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,54 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
-	</view>
+  <view class="content">
+    <image class="logo" src="../../static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }}</text>
+    </view>
+
+    <uni-drawer :visible="true">
+      <view style="padding:30rpx;">
+        <view class="uni-title">抽屉式导航</view>
+      </view>
+    </uni-drawer>
+  </view>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-	export default Vue.extend({
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+import uniDrawer from "@dcloudio/uni-ui";
+import { Component } from "vue-property-decorator";
+import Vue from "vue";
 
-		},
-		methods: {
-
-		}
-	});
+interface Test {
+  readonly title: string;
+}
+@Component({
+  data: (): Test => {
+    return {
+      title: "123",
+    };
+  }
+})
+export default class Index extends Vue {
+  component: { uniDrawer: uniDrawer };
+  mounted() {
+    console.log(this);
+  }
+  replaceInput() {}
+}
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+.content {
+  text-align: center;
+  height: 400upx;
+}
+.logo {
+  height: 200upx;
+  width: 200upx;
+  margin-top: 200upx;
+}
+.title {
+  font-size: 36upx;
+  color: #8f8f94;
+}
 </style>
