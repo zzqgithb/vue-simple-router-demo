f155358f2650d7f2a2b024d6f95caa35b34bdf40
zhangmengping <774623210@qq.com>
2020-02-05 13:52:17 +0800

    添加登录

--- src/pages/login/index/view/index.vue
+++ src/pages/login/index/view/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <view>
-    登录
+  <view class="uni-common-form-style">
+    <view class="uni-common-mt ">
+      <view class="uni-form-item uni-column">
+        <input class="uni-input" type="number" placeholder="请输入账号" />
+      </view>
+      <view class="uni-form-item uni-column">
+        <input
+          class="uni-input"
+          password
+          type="text"
+          placeholder="请输入密码"
+        />
+      </view>
+    </view>
+    <view class="uni-padding-wrap uni-common-mt">
+      <button type="primary" @click="loginIn">登录</button>
+    </view>
   </view>
 </template>
 
 <script type="text/ecmascript-6">
+import ILoginCtrl from "@/pages/login/index/interface/ILoginCtrl";
+
 export default {
     props: {},
     data() {
@@ -17,9 +34,25 @@ export default {
     },
     updated() {
     },
-    methods: {},
+    methods: {
+      loginIn(){
+        console.log('ILoginCtrl',ILoginCtrl);
+      },
+    },
     watch: {},
 };
 </script>
 
-<style rel="stylesheet/scss" lang="scss" scoped></style>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.uni-common-form-style {
+  margin: 10px;
+  .uni-form-item {
+    margin: 10px;
+  }
+  input {
+    border: 1px solid #ccc;
+    height: 40px;
+    border-radius: 8px;
+  }
+}
+</style>
