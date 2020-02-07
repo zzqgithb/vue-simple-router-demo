17fe0deec93e32db61ad283b504b050c13153d9f
zhangmengping <774623210@qq.com>
2020-02-07 11:46:49 +0800

    添加缺少部分

--- src/pages/login/index/view/index.vue
+++ src/pages/login/index/view/index.vue
@@ -21,24 +21,41 @@
         </view>
         <view class="uni-form-item uni-column">
           <text class="jeicon jeicon-lock"></text>
-          <input
-            ref="password"
-            class="uni-input"
-            password
-            type="text"
-            placeholder="密码"
-            placeholder-style="color: #aab2bd;"
-            v-model="loginCtrl.password"
-          />
-          <text class="jeicon jeicon-eye-close"></text>
+          <view v-show="codeShow" class="" style="flex:1;display:flex;">
+            <input
+              ref="password"
+              class="uni-input"
+              password
+              type="text"
+              placeholder="密码"
+              placeholder-style="color: #aab2bd;"
+              v-model="loginCtrl.password"
+            />
+            <text class="jeicon jeicon-eye-close"></text>
+          </view>
+          <view v-show="!codeShow" class="" style="flex:1;display:flex;">
+            <input
+              ref="password"
+              class="uni-input"
+              type="text"
+              placeholder="短信动态码"
+              placeholder-style="color: #aab2bd;"
+              v-model="loginCtrl.password"
+            />
+            <text class="uni-getcode">获取</text>
+          </view>
         </view>
         <view class="uni-common-mt uni-change-login">
-          <text>动态密码登录</text>
+          <text @click="codeShow = !codeShow">动态密码登录</text>
           <text>忘记密码？</text>
         </view>
       </view>
       <view class="uni-padding-wrap uni-common-mt uni-button-login">
         <button type="primary" @click="loginIn">登录</button>
+        <view class="uni-other-login">
+          <text class="jeicon jeicon-qq"></text>
+          <text class="jeicon jeicon-weixin"></text>
+        </view>
       </view>
     </view>
   </view>
@@ -56,6 +73,7 @@ export default class Login extends Vue {
     return{
       username:'',
       password:'',
+      codeShow:true,
     };
   }
   created() {
@@ -113,6 +131,12 @@ export default class Login extends Vue {
           transform: rotate(90deg);
         }
       }
+      .uni-getcode {
+        color: #fff;
+        background: #386bd0;
+        padding: 4rpx 16px;
+        font-size: 20rpx;
+      }
     }
     .uni-change-login {
       width: 600rpx;
@@ -120,12 +144,27 @@ export default class Login extends Vue {
       display: flex;
       justify-content: space-between;
       color: #386bd0;
+      font-size: 32rpx;
     }
     .uni-button-login {
       width: 600rpx;
-      margin: 200rpx auto;
-      box-shadow: 0 4rpx 4rpx 0 rgba(83, 109, 254, 0.44);
-      color: #ffffff;
+      margin: 100rpx auto;
+      button {
+        box-shadow: 0 4rpx 4rpx 0 rgba(83, 109, 254, 0.44);
+        color: #ffffff;
+      }
+      .uni-other-login {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-top: 90rpx;
+        text {
+          flex: 1;
+          text-align: center;
+          color: #cfcfcf;
+          font-size: 60rpx;
+        }
+      }
     }
   }
 }
